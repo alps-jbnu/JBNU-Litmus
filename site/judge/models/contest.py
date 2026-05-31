bf152730f19431a5a91c170678767dd57b36b9b3
mindungil <alswnsrlf12@naver.com>
2026-05-31 23:25:26 +0900

    Add late submission deadline field

--- site/judge/models/contest.py
+++ site/judge/models/contest.py
@@ -99,6 +99,8 @@ class Contest(models.Model):
     problems = models.ManyToManyField(Problem, verbose_name=_('problems'), through='ContestProblem')
     start_time = models.DateTimeField(verbose_name=_('start time'), db_index=True)
     end_time = models.DateTimeField(verbose_name=_('end time'), db_index=True)
+    late_submission_deadline = models.DateTimeField(verbose_name=_('late submission deadline'), blank=True, null=True,
+                                                    db_index=True)
     time_limit = models.DurationField(verbose_name=_('time limit'), blank=True, null=True)
     is_visible = models.BooleanField(verbose_name=_('publicly visible'), default=False,
                                      help_text=_('Should be set even for organization-private contests, where it '
