0f5706633d267dfc3d4a927c11522a132870b39e
Hong Chae Un <hcw88928@naver.com>
2026-07-27 16:57:53 +0900

    Revert "fix: 관리자 페이지 학교 필터 스타일 개선"

--- site/judge/admin/school.py
+++ site/judge/admin/school.py
@@ -1,65 +1,7 @@
 from django.contrib import admin
 from django import forms
-from django.contrib.admin.filters import FieldListFilter
 
 from judge.models import School
-
-
-class CombinedSchoolFilter(FieldListFilter):
-    title = ' '
-    template = 'admin/input_filter/input_filter_school.html'
-
-    def __init__(self, field, request, params, model, model_admin, field_path):
-        super().__init__(field, request, params, model, model_admin, field_path)
-        self.request = request
-        self.params = params
-
-        self.__school_type_lookups = tuple(School.SCHOOL_TYPES)
-        self.__school_type_handles = set(value for value, _ in self.__school_type_lookups)
-
-        self.__is_jbnu_lookups = (('1', '전북대'), ('0', '전북대 아님'))
-        self.__is_active_lookups = (('1', '활성'), ('0', '비활성'))
-        self.__boolean_handles = {'1', '0'}
-
-        self.filter_keys = ['school_type', 'is_jbnu', 'is_active']
-
-    @property
-    def school_type_lookups(self):
-        return self.__school_type_lookups
-
-    @property
-    def is_jbnu_lookups(self):
-        return self.__is_jbnu_lookups
-
-    @property
-    def is_active_lookups(self):
-        return self.__is_active_lookups
-
-    def expected_parameters(self):
-        return ['school_type', 'is_jbnu', 'is_active']
-
-    def choices(self, changelist):
-        yield {
-            'selected': False,
-            'query_string': changelist.get_query_string(remove=self.expected_parameters()),
-            'display': '초기화',
-        }
-
-    def queryset(self, request, queryset):
-        school_type = request.GET.get('school_type')
-        is_jbnu = request.GET.get('is_jbnu')
-        is_active = request.GET.get('is_active')
-
-        if school_type in self.__school_type_handles:
-            queryset = queryset.filter(school_type=school_type)
-
-        if is_jbnu in self.__boolean_handles:
-            queryset = queryset.filter(is_jbnu=(is_jbnu == '1'))
-
-        if is_active in self.__boolean_handles:
-            queryset = queryset.filter(is_active=(is_active == '1'))
-
-        return queryset
 
 
 class CustomActionForm(forms.Form):
@@ -73,9 +15,7 @@ class CustomActionForm(forms.Form):
 
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ['name', 'short_name', 'school_type', 'is_jbnu', 'is_active']
-    list_filter = (
-        ('id', CombinedSchoolFilter),
-    )
+    list_filter = ['school_type', 'is_jbnu', 'is_active']
     search_fields = ['name', 'short_name']
     fields = ('name', 'short_name', 'school_type', 'is_jbnu', 'is_active')
     action_form = CustomActionForm
